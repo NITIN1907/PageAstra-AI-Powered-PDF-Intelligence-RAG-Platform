33f78ea729b459b55d8f256b2a2cde2d98799eaf
NITIN1907 <118448478+NITIN1907@users.noreply.github.com>
2026-09-15 17:18:12 +0530

    Update allowed origins for CORS configuration

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://your-rag-project.vercel.app"
+        "https://page-astra-ai-powered-pdf-intellige-beta.vercel.app/"
     ],
 
     allow_credentials=True,
